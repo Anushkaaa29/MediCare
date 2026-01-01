bcb29041ee1c4ad52ebc3c2b771e0b35341328e2
Anushkaaa29 <anushkapandeyy27@gmail.com>
2026-01-01 17:43:02 +0530

    Vercel serverless setup

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,4 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+export default app;
